5995f32352a67977f1488f6c399b90e1f2cced92
Алексей <electrifyx@gmail.com>
2023-10-07 15:47:21 +0300

    edit botCode

--- index.js
+++ index.js
@@ -19,11 +19,14 @@ bot.setWebHook(webHookUrl);
 
 bot.on("message", (msg) => {
   const {
-    chat: { id, first_name },
+    chat: { id },
+    from: { first_name },
   } = msg;
   bot.sendMessage(id, "My name is bot Mr. McDuck");
 
-  const welcomeMessage = `Привет, ${first_name}! Добро пожаловать в моё гнездо! Может жареных яиц?:)`;
+  const userName =
+    msg.from.first_name || msg.chat.first_name || "Неизвестный юзер";
 
+  const welcomeMessage = `Привет, ${userName}! Добро пожаловать в моё гнездо! Может жареных яиц?:)`;
   bot.sendMessage(id, welcomeMessage);
 });
